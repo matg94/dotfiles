dd97cd2fb2a126eda60ebce8c825f271c989830e
mat <mat.gautron@gmail.com>
2022-01-16 09:12:14 +0000

    Changed to completely new theme and setup

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -2,4 +2,7 @@
 
 sleep 0.5
 compton
-nitrogen --set-zoom-fill --random ~/wallpapers
+
+xrandr --output HDMI-1 --mode 1920x1080 --rate 144 --output DP-0 --auto --right-of HDMI-1
+sudo wpa_supplicant -i wlp6s0 -B -c/etc/wpa_supplicant.conf > /dev/null
+sudo dhclient wlp6s0 > /dev/null
--- copy-dotfiles.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp ./dotfiles/zsh/zshrc /home/$1/.zshrc
-cp ./dotfiles/vim/vimrc /home/$1/.vimrc

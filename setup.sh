94fff78ce8854e5d55184e268939ffe6ebebbb14
matg94 <mat.gautron@gmail.com>
2025-02-23 09:41:28 +0000

    Cleaning up old x configs and adding ghostty

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
+# Packages
+sudo apt update
+sudo apt install curl jq zsh python3-pip pipx
 
+# Ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-CONFIG_DIR=$HOME/.config
+# Install ZSH plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-mkdir -p $CONFIG_DIR/mako && cp -r ./mako $CONFIG_DIR/
+# Kubectl 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Keeper CLI
+pipx install keepercommander
+
+# Path
+pipx ensurepath
